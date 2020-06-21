e41696dad2d8df15217ce93aaff7c75289fef3ed
davutdag <davutdag35@gmail.com>
2020-06-21 22:14:20 +0300

    ikinci commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("ilk commit from local reposýtory");
-
+		System.out.println("ikinci commit from local reposýtory");
 	}
 
 }
